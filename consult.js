7417aaf1a57ac44c8252a4eff3d293f4265b3cbc
joaobelforti <joaopedro.1511@gmail.com>
2021-12-01 14:42:47 -0300

    js-scripts

--- consult.js
+++ consult.js
@@ -6,8 +6,12 @@ const socketPath = "../cardano-src/cardano-node/path/to/db/node.socket";
 
 const cardano = new CardanocliJs({ shelleyGenesisPath , socketPath});
 
-for(let n = 1;n < process.argv[2]; n++){
-	const dir = ["wallet".concat(n.toString())];
+console.log(process.argv[2]+1)
+
+for(let n = 0;n < process.argv[2]; n++){
+	const dir = ["wallet".concat((n+1).toString())];
+
+	console.log("wallet",n+1)
 
 	const wallet = cardano.wallet(dir);
 	
